dad48f55b5513eb7c42041ab0127701e37c08c50
keves <kevinpaulcalle@gmail.com>
2016-05-03 07:46:57 -0500

    primer actualizacion

--- prueba1/src/prueba1/Prueba1.java
+++ prueba1/src/prueba1/Prueba1.java
@@ -15,7 +15,7 @@ public class Prueba1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("pruebas para desarrollo ");
     }
     
 }
